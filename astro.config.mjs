245850f8f7d1e504d59c5a41117b99a8f6fecd3e
Haranadh G <haranadhg@Haranadhs-MacBook-Pro-3.local>
2026-06-09 11:20:27 +0100

    feat(seo): integrate @astrojs/sitemap to automate sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://haranadh.com',
   outDir: 'dist',
   build: {
     format: 'file'
-  }
+  },
+  integrations: [sitemap()]
 });
